0c735eb1954a5c0115700545e3008a0fcaf010dd
John Foley <jfoley@cs.umass.edu>
2018-07-11 09:02:59 -0400

    ditch keras

--- forest2gen/train_keras.py
+++ forest2gen/train_keras.py
@@ -1,29 +1,33 @@
-from keras.models import Sequential
-from keras.layers import Dense, Input
-from keras import backend as K
-import argparse
-import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import argparse, random, json
 import numpy as np
+import itertools
+from tqdm import tqdm
 from forest2gen import smart_reader, load_ranklib_model, split_points_to_generator
 from ranklib2numpy import load_ranklib_file
 from sklearn.metrics import average_precision_score
 from collections import defaultdict
 
+class ApproximateRanker(nn.Module):
+    def __init__(self, dim, layers=[500,100]):
+        super(ApproximateRanker, self).__init__()
+        steps = []
+        for i in range(len(layers)):
+            if i == 0:
+                steps.append(nn.Linear(dim, layers[i]))
+                steps.append(nn.ReLU6())
+            else:
+                steps.append(nn.Linear(layers[i-1], layers[i]))
+                steps.append(nn.ReLU6())
+        steps.append(nn.Linear(layers[-1], 1))
+        steps.append(nn.ReLU6())
+        self.forward_layers = nn.Sequential(*steps)
 
-def relu6(x):
-    return K.relu(x, max_value=6)
-
-
-def create_dnn_model(D, layers=[500, 100], optimizer='adam', loss='mean_absolute_error', activation=relu6):
-    assert(len(layers) >= 1)
-    model = Sequential()
-    model.add(Dense(layers[0], activation=activation, input_shape=(D,)))
-    for layer in layers[1:]:
-        model.add(Dense(layer, activation=activation))
-    model.add(Dense(1))
-    model.compile(optimizer=optimizer, loss=loss)
-    return model
-
+    def forward(self, xs):
+        return self.forward_layers(xs)
 
 def compute_aps(pred_y, test_y, test_qids):
     pred = defaultdict(list)
@@ -43,23 +47,39 @@ def compute_aps(pred_y, test_y, test_qids):
 
     return np.array(aps)
 
+def batch(lst, n):
+    l = len(lst)
+    for ndx in range(0, l, n):
+        yield lst[ndx:min(ndx + n, l)]
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Train a DNN from a LTR Forest Model.')
     parser.add_argument('input_model')
     parser.add_argument('stats_file')
+    parser.add_argument('train_data')
     parser.add_argument('test_data')
     parser.add_argument('output_model')
     parser.add_argument('--quiet', required=False,
                         action='store_true', default=False)
+    # Note that our batches are actually twice this size.
     parser.add_argument('--batch_size', required=False, type=int, default=32)
-    parser.add_argument('--batch_steps', required=False, type=int, default=200)
-    parser.add_argument('--num_batches', required=False, type=int, default=20)
     args = parser.parse_args()
 
+    print('load test data')
     test_X, test_y, test_qids, names = load_ranklib_file(args.test_data)
+    print('load train data')
+    train_X, train_y, _, _ = load_ranklib_file(args.test_data)
+    print('load ranklib ensemble')
     ensemble = load_ranklib_model(args.input_model)
+
+    training_data = []
+    for i in range(len(train_y)):
+        xv = train_X[i,:]
+        y_forest = ensemble.eval(xv)
+        training_data.append( (y_forest, xv) )
+    print('collected predictions')
 
     ensemble_ys = []
     for i in range(len(test_qids)):
@@ -73,18 +93,19 @@ if __name__ == '__main__':
 
     D = len(fstats)+1
     print("fstats.size={0}".format(len(fstats)))
-    model = create_dnn_model(D, [500, 100])
+    model = ApproximateRanker(D, [500, 100])
 
     generate_fn = split_points_to_generator(ensemble, fstats)
 
-    for b in range(args.num_batches):
-        train_loss = []
-        for i in range(args.batch_steps):
-            X, y = generate_fn(args.batch_size)
-            loss = model.train_on_batch(X, y)
-            train_loss.append(loss)
-        print('Train[{2},{1}].Loss: {0}'.format(np.mean(loss), i, b))
-        # predict on test data:
-        pred_y = model.predict(test_X)
-        aps = compute_aps(pred_y, test_y, test_qids)
-        print('Test.mAP: {0}'.format(np.mean(aps)))
+    model.train()
+    random.shuffle(training_data)
+    progress = tqdm(batch(training_data, args.batch_size))
+    for minibatch in progress:
+        train_ys = torch.tensor([y for (y,_) in minibatch], dtype=torch.float)
+        train_xs = torch.cat([torch.tensor(x) for (_,x) in minibatch], axis=0)
+        cover_x, cover_y = generate_fn(args.batch_size)
+        batch_xs = torch.cat([torch.from_numpy(cover_x), train_xs], axis=0)
+        batch_ys = torch.cat([torch.from_numpy(cover_y), train_ys], axis=0)
+        print(batch_xs.shape, batch_ys.shape)
+        #aps = compute_aps(pred_y, test_y, test_qids)
+        #print('Test.mAP: {0}'.format(np.mean(aps)))
